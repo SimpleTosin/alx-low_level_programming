825abb6e54e6cd6bd1b84a22cd08c535ece4d6d1
SimpleTosin <righdavid@gmail.com>
2022-12-22 08:20:48 -0800

    encodes a string into 1337.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * leet - encodes a string into 1337
+ *
+ *   * @s: string to be encoded
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+char *leet(char *s)
+
+{
+
+		int q;
+
+			int r;
+
+				char v[] = "aAeEoOtTlL";
+
+					char w[] = "4433007711";
+
+
+
+						for (q = 0; *(s + q); q++)
+
+								{
+
+											for (r = 0; r <= 9; r++)
+
+														{
+
+																		if (v[r] == s[q])
+
+																							s[q] = w[r];
+
+																				}
+
+												}
+
+							return (s);
+
+}
