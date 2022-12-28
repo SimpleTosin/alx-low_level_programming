b48dc9d8bfc4a9ba37fb4d7ce2dfde6e5d80fc2e
SimpleTosin <righdavid@gmail.com>
2022-12-28 14:20:47 -0800

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,16 @@ for (n1 = '0'; n1 <= '8'; n1++)
 {
 for (n2 = '1'; n2 <= '9'; n2++)
 {
-if (n1 < n2)								
+if (n1 < n2)															
 {				
 putchar(n1);					
-putchar(n2);								
-if (n1 == '8' && n2 == '9')continue;						putchar(',');									putchar(' ');
-}
-}										}
-putchar('\n');
-return (0);
+putchar(n2);															
+if (n1 == '8' && n2 == '9')continue;																																		putchar(',');
+																																									putchar(' ');
+																																												}
+																		}
+										}
+					putchar('\n');
+
+					return (0);
 }
